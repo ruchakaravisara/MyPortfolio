68d73bc2e52f88108f6063e0c22c6bec2a9f5a73
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-06-27 15:57:49 +0530

    nothing

---
+++ assets/Js/loader.js
@@ -0,0 +1,15 @@
+
+/*
+    window.addEventListener('load', function () {
+    // Show the loader initially
+    document.getElementById('loader').style.display = 'flex';
+    //document.getElementById('html').style.display = 'none';
+
+    // Hide the loader and show the content after 5 seconds
+    setTimeout(function () {
+    document.getElementById('loader').style.display = 'none';
+    //document.getElementById('html').style.display = 'block';
+}, 5000); // 5 seconds (5000 milliseconds)
+});
+*/
+
